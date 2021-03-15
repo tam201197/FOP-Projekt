efbccb85616876b5e1c27a2053a3d4e306103757
nhuhuyennc <56841149+nhuhuyennc@users.noreply.github.com>
2021-03-15 15:56:39 +0100

    Highscores update

--- Projektgruppe_XXX/src/main/fop/view/menu/HighscoreView.java
+++ Projektgruppe_XXX/src/main/fop/view/menu/HighscoreView.java
@@ -1,7 +1,9 @@
 package fop.view.menu;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.time.format.DateTimeFormatter;
 
@@ -20,8 +22,6 @@ import fop.view.MainFrame;
 
 public class HighscoreView extends MenuView{
 	
-	
-
 
 	public HighscoreView(MainFrame window) {
 		super(window, "Highscores");
@@ -31,19 +31,19 @@ public class HighscoreView extends MenuView{
 	@Override
 	protected void addContent(JPanel contentPanel) {
 		// TODO Auto-generated method stub
-		/*GridBagConstraints aboutScoresTable = new GridBagConstraints();
+		contentPanel.setLayout(new GridBagLayout());
+		GridBagConstraints aboutScoresTable = new GridBagConstraints();
 		aboutScoresTable.weightx = 1.0;
 		aboutScoresTable.weighty = 1.0;
 		aboutScoresTable.fill = GridBagConstraints.BOTH;
 		aboutScoresTable.insets = new Insets(0, 2, 2, 2);
 		aboutScoresTable.gridx = 0;
-		aboutScoresTable.gridy = 0;*/
+		aboutScoresTable.gridy = 0;
 		
 		String[] headers = new String[] {
 	            "Datum und Uhrzeit", "Name", "Punkte"
 	     };
-		ScoreEntry[] a = ScoreEntryIO.loadScoreEntries().toArray(new ScoreEntry[0]); 
-
+		ScoreEntry[] a = ScoreEntryIO.loadScoreEntries().toArray(new ScoreEntry[0]);
 		String [][] data = new String [a.length][3];
 		
 		for(int x = 0;x<a.length;x++) {
@@ -53,23 +53,18 @@ public class HighscoreView extends MenuView{
 			data[x][2] = String.valueOf(a[x].getScore());
 		}
 		JTable scoreTable = new JTable(data, headers);
-		JTableHeader header = scoreTable.getTableHeader();
-		header.setBackground(contentPanel.getBackground());
-		((DefaultTableCellRenderer)header.getDefaultRenderer()).setHorizontalAlignment(JLabel.CENTER);
-				
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setPreferredSize(new Dimension(700,275));
-		scrollPane.getViewport().add(scoreTable);
-		
-		scoreTable.setAutoCreateRowSorter(true);
-		
-		contentPanel.add(scrollPane);
+		JScrollPane scrollPane = new JScrollPane(scoreTable);
 		
 		
+		JTableHeader theader = scoreTable.getTableHeader();
+		theader.setBackground(this.getBackground());
+		((DefaultTableCellRenderer)theader.getDefaultRenderer()).setHorizontalAlignment(JLabel.CENTER);			
 		
 		
+				scoreTable.setAutoCreateRowSorter(true);
+		contentPanel.add(scrollPane, aboutScoresTable);
 		
-		
+
 		//Back Botton
 		GridBagConstraints rightImageConstraints = new GridBagConstraints();
 		rightImageConstraints.insets = new Insets(2, 2, 0, 2);
@@ -78,6 +73,13 @@ public class HighscoreView extends MenuView{
 		JButton backButton = createButton("Zurück");
 		backButton.addActionListener(evt -> getWindow().setView(new MainMenu(getWindow())));
 		contentPanel.add(backButton, rightImageConstraints);
+	}
+
+	@Override
+	public void onResize() {
+		// TODO Auto-generated method stub
+		super.onResize();
+		
 	}
 
 }
